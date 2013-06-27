d4e7e32b121f90d92ba9bd47ed328fe8b6fd91e1
Scott Travis <scott.w.travis@gmail.com>
2013-06-26 23:50:03 -0500

    Working.

--- functions/number_format_abbr.php
+++ functions/number_format_abbr.php
@@ -2,17 +2,16 @@
 
 function number_format_abbr($n, $decimals = 1)
 {
-    // first strip any formatting;
-    $n = (0+str_replace(",","",$n));
+    // strip formatting
+    $n = (0 + str_replace(',', '', $n));
    
-    // is this a number?
-    if(!is_numeric($n)) return false;
+    // check if number
+    if (!is_numeric($n)) return false;
    
-    // now filter it;
-    if($n>=1000000000000) return round(($n/1000000000000),$decimals).'T';
-    else if($n>=1000000000) return round(($n/1000000000),$decimals).'B';
-    else if($n>=1000000) return round(($n/1000000),$decimals).'M';
-    else if($n>=1000) return round(($n/1000),$decimals).'K';
-    
-    return number_format($n);
+    // process
+    if ($n >= 1000000000000) return round(($n / 1000000000000), $decimals).'T';
+    elseif ($n >= 1000000000) return round(($n / 1000000000), $decimals).'B';
+    elseif ($n >= 1000000) return round(($n / 1000000), $decimals).'M';
+    elseif ($n >= 1000) return round(($n / 1000), $decimals).'K';
+    else return number_format($n);
 }
